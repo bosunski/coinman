ac826603fb5e17411ef8517a9707a0edc7b568ac
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-09-02 07:18:14 -0700

    Update index.php
    called to the index file... using Ajax

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Load Coinman</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
 
-require('class/Poloniex.php');
-require('class/Coinman.php');
-$config = include('config.php');
 
-// Crates an instance of CoinMan
-$cm = new Coinman($config['api_key'], $config['api_secret']);
 
-// We Run CoinMan
-$cm->runCoinMan();
+<script type="text/javascript">
+	$("document").ready(function (){
+		var data = 'all';
+		$.ajax({
+			type: "GET",
+			url: "class/load-data.php",
+			dataType: "json",
+			data: {
+				data: data
+			},
+			success: function(data){
+				// here data is alread in Json from the dataType set
+			}
+		});
+	});
+
+</script>
+</body>
+</html>
