a343e835c8bba61e40ba44f75928294b7e72c6f1
Ekpoto Liberty <ekpoto.liberty@gmail.com>
2017-09-02 07:22:36 -0700

    Update load-data.php
    get  request catch from index

--- class/load-data.php
+++ class/load-data.php
@@ -10,9 +10,12 @@ $cm = new Coinman($config['api_key'], $config['api_secret']);
 // We Run CoinMan
 $cm->runCoinMan();
 
-// this pages only return loaded json
-$load_data = $cm->loadData();
+// get request from the home page query 
+if(isset($_GET['data'])){
+	// this pages only return loaded json
+	$load_data = $cm->loadData();
 
-// if the data is already in Json decode it
-echo json_decode($load_data);
+	// if the data is already in Json decode it
+	echo json_decode($load_data);
+}
 ?>
